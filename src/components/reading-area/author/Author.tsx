3ec2f03cf3c4141006cb3b3605ec8dd59f1049f2
iSathsara <isuru.dex@gmail.com>
2021-02-17 16:48:15 +0530

    book section UI designed, add book form created.

--- src/components/reading-area/author/Author.tsx
+++ src/components/reading-area/author/Author.tsx
@@ -4,13 +4,13 @@ import {Col, Row} from "react-bootstrap";
 const Author : React.FC = () => {
 
     return(
-        <Row className="author-details py-1">
-            <Col xs={9}>
+        <Row className="author-details py-1 ml-0 mr-4">
+            <Col xs={9} className="pl-0">
                <label>1.Author 1</label>
             </Col>
             <Col xs={3} className="text-right">
                 <i className="feather icon-edit text-warning mr-3" />
-                <i className="feather icon-trash-2 text-danger mr-5"/>
+                <i className="feather icon-trash-2 text-danger mr-2"/>
             </Col>
         </Row>
     )
